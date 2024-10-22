1b124faa44584042ca4974522b1d60a40dd0e069
Gerardo Gonzalez <Gerardo.Gonzalez@techfino.com>
2024-10-21 22:05:52 -0300

    price adding

--- server/api-util/lineItems.js
+++ server/api-util/lineItems.js
@@ -121,11 +121,11 @@ const getDateRangeQuantityAndLineItems = (orderData, code) => {
  */
 exports.transactionLineItems = (listing, orderData, providerCommission, customerCommission) => {
   const publicData = listing.attributes.publicData;
-   const { bookingStart, bookingEnd } = orderData;
+   //const { bookingStart, bookingEnd } = orderData;
 /*    console.log('bookingStart', bookingStart);
    console.log('bookingStart', bookingStart);
    console.log('publicData', publicData); */
-  //const unitPrice = listing.attributes.price;
+  const unitPrice = listing.attributes.price;
  //  const basePrice = listing.attributes.price;
 
 /*    const { startDateHighSeason, endDateHighSeason, startDateMediumSeason, EndDateMediumSeason, startDateLowSeason, endDateLowSeason, 
